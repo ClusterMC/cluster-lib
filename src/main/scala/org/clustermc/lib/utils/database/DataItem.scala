5b15a4968e059645539f282851a82e42972c42c2
Carter Gale <buildfresh@gmail.com>
2015-11-10 17:21:45 -0800

    Added dataitem

---
+++ src/main/scala/org/clustermc/lib/utils/database/DataItem.scala
@@ -0,0 +1,14 @@
+package org.clustermc.lib.utils.database
+
+import java.util.UUID
+
+/*
+ * Copyright (C) 2013-Current Carter Gale (Ktar5) <buildfresh@gmail.com>
+ * 
+ * This file is part of Hub.
+ * 
+ * Hub can not be copied and/or distributed without the express
+ * permission of the aforementioned owner.
+ */
+
+abstract class DataItem(itemId: UUID)
