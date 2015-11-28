e29d89b31dbe92a63e4fb1d1b1c3fb9a52494667
Carter Gale <buildfresh@gmail.com>
2015-11-27 22:31:31 -0800

    Revised channel help and made hotbars more discriminatory against inventories

--- src/main/scala/org/clustermc/lib/chat/channel/commands/ChannelHelpCommand.scala
+++ src/main/scala/org/clustermc/lib/chat/channel/commands/ChannelHelpCommand.scala
@@ -15,21 +15,17 @@ import org.clustermc.lib.utils.StringUtil
 object ChannelHelpCommand {
 
   val help = StringUtil.colorArray(Array(
-    "&7===&b&lChannel Help&7===",
-    "  &bhelp&7|&bh&7|&b? &7-- displays the help commands",
-    "    &7eg. &b/channel help",
-    "&7===&b&lChannel Sub&7===",
-    "  &bsub&7|&bjoin&7|&bview &7-- lists subscribed channels",
-    "    &7eg. &b/channel sub &7(&cname&7) -- joins selected channel",
-    "&7===&b&lChannel Focus&7===",
-    "  &bfocus&7|&bf&7|&btalk&7|&bt &7-- lists the name of focused channels you're currently in",
-    "    &7eg. &b/channel focus &7(&cname&7) -- subscribes and focuses on selected channel",
-    "&7===&b&lChannel Leave&7===",
-    "  &bleave&7|&bexit&7|&bquit &7-- leaves currently focused channel",
-    "    &7eg. &b/channel leave &7(&cname&7) -- leaves currently focused channel",
-    "&7===&b&lChannel Quick-Send&7===",
-    "  &7<&cchannel&7> <&cmessage&7> -- sends a quick message to a channel",
-    "    &7eg. &b/channel general &7(&cmessage&7) -- quickly sends a message to a channel"))
+    "&f=====&b&lChannel Subscribing&f=====",
+    "  &f/ch &bsub &7-- lists subscribed channels",
+    "  &f/ch &fsub &f(&bchannel&f)&7 -- join &bchannel&7 as a listener",
+    "&f=====&b&lChannel Focus&f=====",
+    "  &f/ch &bf &7-- lists the name of focused channels you're currently in",
+    "  &f/ch &bf &f(&bchannel&f)&7 -- join &bchannel&7 as a speaker",
+    "&f=====&b&lChannel Leave&f=====",
+    "  &f/ch &bquit &7-- leaves currently focused channel",
+    "  &f/ch &bquit &f(&bchannel&f)&7 -- leaves currently focused channel",
+    "&f=====&b&lChannel Quick-Send&f=====",
+    "  &f/ch &f(&bchannel&f) &f(&bmessage&f) -- sends &bmessage &7to &bchannel"))
 
   def apply(sender: Player) = sender.sendMessage(help)
 }
